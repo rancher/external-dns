d59cd737bd4a0c732ed54900a32209894cad84d9
Reynolds <justin.reynolds@charter.com>
2017-07-10 11:25:29 -0600

    supressing error message

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -213,6 +213,7 @@ func (m *MetadataClient) getContainersDnsRecords(dnsEntries map[string]utils.Met
 						ourFqdns[fqdn] = struct{}{}
 					}else if err != nil{
 						logrus.Warnf("Regex matching error: %v", err)
+						continue
 					//Checks to see if there is a full domain name already matching the root domain name
 					//If there is, we just want to register it to dns
 					//If not, we still need to append our root domain name and probably all the other stuff
