a3425b32d81341ebece52d2fa80447868259e119
Alena Prokharchyk <alena@rancher.com>
2018-03-13 13:56:59 -0700

    Revert "Switch to API v2"
    This reverts commit 724917dc83efbb9746c98af42cd7b787fd1daf61.

--- providers/dnsimple/dnsimple.go
+++ providers/dnsimple/dnsimple.go
@@ -11,10 +11,12 @@ import (
 	"github.com/juju/ratelimit"
 	"github.com/rancher/external-dns/providers"
 	"github.com/rancher/external-dns/utils"
+	api "github.com/weppos/go-dnsimple/dnsimple"
 )
 
 type DNSimpleProvider struct {
-	client    *dnsimple.Client
+	client    *api.Client
+	client2   *dnsimple.Client
 	accountID string
 	root      string
 	limiter   *ratelimit.Bucket
@@ -25,10 +27,14 @@ func init() {
 }
 
 func (d *DNSimpleProvider) Init(rootDomainName string) error {
-	var oauthToken string
+	var email, apiToken, oauthToken string
 
-	if len(os.Getenv("DNSIMPLE_EMAIL")) > 0 {
-		return fmt.Errorf("DNSimple API v2 requires an account identifier and the new OAuth token. Please upgrade your configuration.")
+	if email = os.Getenv("DNSIMPLE_EMAIL"); len(email) == 0 {
+		return fmt.Errorf("DNSIMPLE_EMAIL is not set")
+	}
+
+	if apiToken = os.Getenv("DNSIMPLE_TOKEN"); len(apiToken) == 0 {
+		return fmt.Errorf("DNSIMPLE_TOKEN is not set")
 	}
 
 	if oauthToken = os.Getenv("DNSIMPLE_TOKEN"); len(oauthToken) == 0 {
@@ -36,10 +42,11 @@ func (d *DNSimpleProvider) Init(rootDomainName string) error {
 	}
 
 	d.root = utils.UnFqdn(rootDomainName)
-	d.client = dnsimple.NewClient(dnsimple.NewOauthTokenCredentials(oauthToken))
+	d.client = api.NewClient(apiToken, email)
+	d.client2 = dnsimple.NewClient(dnsimple.NewOauthTokenCredentials(oauthToken))
 	d.limiter = ratelimit.NewBucketWithRate(1.5, 5)
 
-	whoamiResponse, err := d.client.Identity.Whoami()
+	whoamiResponse, err := d.client2.Identity.Whoami()
 	if err != nil {
 		return fmt.Errorf("DNSimple Authentication failed: %v", err)
 	}
@@ -48,7 +55,7 @@ func (d *DNSimpleProvider) Init(rootDomainName string) error {
 	}
 	d.accountID = strconv.Itoa(whoamiResponse.Data.Account.ID)
 
-	_, err = d.client.Zones.GetZone(d.accountID, d.root)
+	_, err = d.client2.Zones.GetZone(d.accountID, d.root)
 	if err != nil {
 		return fmt.Errorf("Failed to get zone for '%s': %v", d.root)
 	}
@@ -63,7 +70,7 @@ func (*DNSimpleProvider) GetName() string {
 
 func (d *DNSimpleProvider) HealthCheck() error {
 	d.limiter.Wait(1)
-	_, err := d.client.Identity.Whoami()
+	_, err := d.client2.Identity.Whoami()
 	return err
 }
 
@@ -82,7 +89,7 @@ func (d *DNSimpleProvider) AddRecord(record utils.DnsRecord) error {
 			Content: rec,
 		}
 		d.limiter.Wait(1)
-		_, err := d.client.Zones.CreateRecord(d.accountID, d.root, recordInput)
+		_, err := d.client2.Zones.CreateRecord(d.accountID, d.root, recordInput)
 		if err != nil {
 			return fmt.Errorf("DNSimple API call has failed: %v", err)
 		}
@@ -95,7 +102,7 @@ func (d *DNSimpleProvider) findRecords(record utils.DnsRecord) ([]dnsimple.ZoneR
 	var zoneRecords []dnsimple.ZoneRecord
 
 	d.limiter.Wait(1)
-	recordsResponse, err := d.client.Zones.ListRecords(d.accountID, d.root, nil)
+	recordsResponse, err := d.client2.Zones.ListRecords(d.accountID, d.root, nil)
 	if err != nil {
 		return zoneRecords, fmt.Errorf("DNSimple API call has failed: %v", err)
 	}
@@ -127,7 +134,7 @@ func (d *DNSimpleProvider) RemoveRecord(record utils.DnsRecord) error {
 
 	for _, zoneRecord := range zoneRecords {
 		d.limiter.Wait(1)
-		_, err := d.client.Zones.DeleteRecord(d.accountID, d.root, zoneRecord.ID)
+		_, err := d.client2.Zones.DeleteRecord(d.accountID, d.root, zoneRecord.ID)
 		if err != nil {
 			return fmt.Errorf("DNSimple API call has failed: %v", err)
 		}
@@ -140,7 +147,7 @@ func (d *DNSimpleProvider) GetRecords() ([]utils.DnsRecord, error) {
 	var records []utils.DnsRecord
 
 	d.limiter.Wait(1)
-	recordsResponse, err := d.client.Zones.ListRecords(d.accountID, d.root, nil)
+	recordsResponse, err := d.client2.Zones.ListRecords(d.accountID, d.root, nil)
 	if err != nil {
 		return records, fmt.Errorf("DNSimple API call has failed: %v", err)
 	}
